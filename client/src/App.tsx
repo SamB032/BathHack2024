e490aa296b8448b85f1397e183050130bd22cac2
Sushil Bhandari <zoroat3@gmail.com>
2024-04-14 11:20:31 +0100

    Finished thing pull. Maybe? Probably not

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
-import {  useState } from 'react';
 import './App.css'
-import Navbar from './components/navbar'
+import {  useEffect, useState } from 'react';
 import { sendDataToAPIKMeans, sendDataToAPINN } from '../utils/sendData';
+import Navbar from './components/navbar'
 import LinearRegGraph from './components/linearRegGraph';
 import LinearRegOutputs from './components/linearRegOuput';
 import LinearRegParams from './components/linearRegParams';
@@ -17,6 +17,65 @@ function App() {
   const [errorValue, setErrorValue] = useState(0);
   const [kToConsider,setKToConsider]= useState(0);
   const [distances, setDistances] = useState<number []>([])
+  const [videoToDisplay, setVideoToDisplay] = useState("");
+  const [howToUseText, setHowToUseText] = useState("");
+  const [modelExplaination, setModelExplaination] = useState("");
+  
+  useEffect(()=>{
+    switch (activePage){
+      case "Linear Regression":
+        setHowToUseText(`        
+        <div style="margin-bottom: 10px; font-size:1.3em; text-align:left">
+        <strong>Point Selection: </strong> Click anywhere on the plot to insert a new data point.
+        </div>
+        <div style="margin-bottom: 10px; font-size:1.3em; text-align:left">
+        <strong>Set the type of the desired error function:</strong> The best-fit line will adjust accordingly to minimize the error on the current data points. The value is also displayed.
+        </div>`);
+        setModelExplaination("Linear regression is a statistical method used to model the relationship between a dependent variable and one or more independent variables. It assumes a linear relationship between the variables, aiming to find the best-fitting line that minimizes the differences between the observed and predicted values. Once the model is trained on a dataset, it can be used to predict the dependent variable's value based on new input values of the independent variables.");
+        setVideoToDisplay("https://www.youtube.com/embed/CtsRRUddV2s")
+        break;
+      
+      case "Nearest Neighbour":
+        setHowToUseText(`
+        <div style="margin-bottom: 10px; font-size:1.3em; text-align:left">
+          <strong>Point Selection: </strong> Click anywhere on the plot to insert a new data point.
+        </div>
+        <div style="margin-bottom: 10px; font-size:1.3em; text-align:left">
+          <strong>Draw lines to K nearest neighbors:</strong> When a new data point is added, the system will draw lines connecting it to its K nearest neighbors and display their distances.
+        </div>
+      `);
+        setModelExplaination("K Nearest Neighbors is a simple and intuitive machine learning algorithm used for classification and regression tasks. In KNN, the prediction for a new data point is based on the majority class or average value of its k nearest neighbors in the feature space. The nearest neighbors are determined based on a distance metric, commonly Euclidean distance. KNN does not involve explicit training; instead, it stores the entire training dataset and calculates distances at prediction time.");
+        setVideoToDisplay("https://www.youtube.com/embed/0p0o5cmgLdE")
+        break;
+      
+      case "K-means":
+        setHowToUseText(`
+        <div style="margin-bottom: 10px; font-size:1.3em; text-align:left">
+          <strong>Select the Algorithm:</strong> Choose the algorithm you want to visualize from the dropdown menu. For example, select "K-means" to visualize the K-means clustering algorithm.
+        </div>
+        <div style="margin-bottom: 10px; font-size:1.3em; text-align:left">
+          <strong>Set Parameters:</strong> If applicable, set any parameters required by the algorithm. For K-means, you can set the number of clusters you want the algorithm to identify.
+        </div>
+        <div style="margin-bottom: 10px; font-size:1.3em; text-align:left">
+          <strong>Interact with the Visualization:</strong> For K-means clustering, click on the data points to interactively assign them to different clusters. As you click on a point, the visualization will update in real-time to show which cluster the data point belongs to. The colors of the points will change dynamically to represent the clustering process.
+        </div>
+        <div style="margin-bottom: 10px; font-size:1.3em; text-align:left">
+          <strong>Observe Cluster Formation:</strong> Watch as the algorithm dynamically updates the clusters based on your interactions. The visualization will provide insights into how the clusters are forming and how the data points are grouped together.
+        </div>
+        <div style="margin-bottom: 10px; font-size:1.3em; text-align:left ">
+          <strong>Experiment and Learn:</strong> Feel free to experiment with different parameter settings and interaction techniques to gain a deeper understanding of how the algorithm works and how it can be applied to your data.
+        </div>
+      `);
+        setModelExplaination("K-means clustering is an unsupervised machine learning algorithm used to partition a dataset into a predetermined number of clusters (k). The algorithm iteratively assigns each data point to the nearest cluster centroid and recalculates the centroids based on the mean of the data points in each cluster. This process continues until the centroids stabilize or a specified number of iterations is reached. K-means aims to minimize the intra-cluster variance, resulting in compact and well-separated clusters. It is widely used for data exploration, pattern recognition, and segmentation tasks.");
+        setVideoToDisplay("https://www.youtube.com/embed/R2e3Ls9H_fc")
+        break;
+    } 
+  },[activePage])
+
+  const renderHTML = (html: string) => {
+    return <div dangerouslySetInnerHTML={{ __html: html }} />;
+  };
+
 
   const getKNNDistances = (distances:number[]) => {
     setDistances(distances)
@@ -105,21 +164,31 @@ function App() {
               </div>}
           <div>  </div>
       </div>
-      <div style={{width:'100vw',height:'500px', backgroundColor:'rgba(65,100,100,0.25)',display:'flex',flexDirection:'column',alignItems:'center'}}>
-        <p className="desc-text"> I'm confused what is machine learning ?</p>
-        <p className="desc-text-inner">Explanation</p>
-        <p className="desc-text">How do I use this ?</p>
-        <p className="desc-text-inner">Explanation</p>
-        <p className="desc-text"> Where can I see the code ?</p>
-        <p className="desc-text-inner">Explanation</p>
-        <p className="desc-text"> I'm still confused ?</p>
-          <iframe width="100px" height="100000px"  src="https://www.youtube.com/embed/0p0o5cmgLdE" 
-            title="K Nearest Neighbors | Intuitive explained | Machine Learning Basics" 
-            frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-            referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-        <p className="desc-text-inner">Explanation</p>
-      </div>
-      <div style={{width:'100vw',height:'500px', backgroundColor:'rgba(127, 150, 151, 0.14)',borderTop:'5px solid black',}}>
+      
+      <div style={{ textAlign: "center" }}>
+      <h1>I'm Confused</h1>
+      <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+        <div style={{marginRight:"20%", marginLeft:"20%"}}>
+          {/* <h3 style={{ margin: "10px 0" }}> I'm confused what is machine learning?</h3> */}
+          
+          <h2 style={{ margin: "10px 0" }}>How do I use this?</h2>
+          {renderHTML(howToUseText)}
+          
+          <h2 style={{ margin: "5px 0", fontStyle: "italic" }}>Model Explaination</h2>
+          <p style={{fontSize:"1.3em"}}> {modelExplaination} </p>
+          
+          <h2 style={{ margin: "10px 0" }}>I'm still confused?</h2>
+          <div style={{ position: "relative", width: "1200px", paddingBottom: "56.25%" }}>
+            <iframe src={videoToDisplay} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen style={{ position: "absolute", top: "0", left: "0", width: "100%", height: "100%" }}></iframe>
+          </div>
+        </div>
+  </div>
+</div>
+
+
+
+       <div> <p className="desc-text-inner">Explanation</p> </div>
+      <div style={{width:'100vw', height:'500px', backgroundColor:'rgba(127, 150, 151, 0.14)',borderTop:'5px solid black',}}>
       BathHack2024
       </div>
     </div>
